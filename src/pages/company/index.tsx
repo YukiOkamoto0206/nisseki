1a5e676d8537b0f138b662da1db917f8de2e405d
Yuki Okamoto <okamotoyu@crunchtimer.jp>
2021-05-31 01:50:29 +0900

    refs: マップの文字削除

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -15,24 +15,6 @@ const Index: React.FC = () => {
     lng: 132.309104112847
   }
 
-  const positionNisseki = {
-    lat: 34.33569522613816,
-    lng: 132.309104112847
-  }
-
-  const divStyle = {
-    background: "white",
-    fontSize: 7.5
-  }
-
-  const [size, setSize] = useState<undefined | google.maps.Size>(undefined)
-  const infoWindowOptions = {
-    pixelOffset: size
-  }
-  const createOffsetSize = () => {
-    return setSize(new window.google.maps.Size(0, -45))
-  }
-
   return (
     <Layout title={"会社情報"}>
       <div className={"text-center m-6"}>
@@ -44,15 +26,9 @@ const Index: React.FC = () => {
         <Info title={"所在地"} content={"広島県廿日市市阿品台東１−２"} />
         <LoadScript
           googleMapsApiKey={process.env.NEXT_PUBLIC_API_KEY as string}
-          onLoad={() => createOffsetSize()}
         >
-          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
-            <Marker position={positionNisseki} />
-            <InfoWindow position={positionNisseki} options={infoWindowOptions}>
-              <div className={"text-center"} style={divStyle}>
-                <p>日赤本社</p>
-              </div>
-            </InfoWindow>
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
+            <Marker position={center} />
           </GoogleMap>
         </LoadScript>
         <p className={"text-white text-xs mt-1"}>
